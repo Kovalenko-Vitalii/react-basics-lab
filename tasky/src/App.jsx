d7552aab7fa25a2a6f5398b80fe23c42fa8aa496
Kovalenko Vitalii <vitkovolenko@gmail.com>
2025-09-16 11:05:33 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Task from './components/Task';
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" />
+      <Task title="Laundry" deadline="Tomorrow" description = "Fold laundry and put away"/>
+      <Task title="Tidy" deadline="Today" />
+    </div>
+  );
+}
+
+export default App;
